309ff898e85cf784093a9c1ab89d4ba0c72a0da8
Matthias Noback <matthiasnoback@gmail.com>
2015-01-14 10:52:03 +0100

    Move Command and Event to MessageBus

---
+++ src/Type/Command.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SimpleBus\Message\Type;
+
+use SimpleBus\Message\Message;
+
+/**
+ * A particular type of message: it is imperative by nature.
+ */
+interface Command extends Message
+{
+}
---
+++ src/Type/Event.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SimpleBus\Message\Type;
+
+use SimpleBus\Message\Message;
+
+/**
+ * A particular type of message: it is informational by nature.
+ */
+interface Event extends Message
+{
+}
