b85b14994fdee06d5aa3ea276ad23baee67619ea
Maks Rafalko <b0rn@list.ru>
2017-01-31 00:33:24 +0300

    Reuse trait instead of duplicating code

--- src/Recorder/PublicMessageRecorder.php
+++ src/Recorder/PublicMessageRecorder.php
@@ -4,31 +4,5 @@ namespace SimpleBus\Message\Recorder;
 
 class PublicMessageRecorder implements RecordsMessages
 {
-    private $messages = [];
-
-    /**
-     * {@inheritdoc}
-     */
-    public function recordedMessages()
-    {
-        return $this->messages;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function eraseMessages()
-    {
-        $this->messages = [];
-    }
-
-    /**
-     * Record a message.
-     *
-     * @param object $message
-     */
-    public function record($message)
-    {
-        $this->messages[] = $message;
-    }
+    use PrivateMessageRecorderCapabilities;
 }
