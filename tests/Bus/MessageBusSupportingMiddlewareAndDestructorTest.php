7c1705512ecb1f28a929a27b12081c54e0ed1e32
Menno Holtkamp <menno.holtkamp@shopforce.nl>
2017-09-30 14:35:16 +0200

    Prevent use of an anonymous class which is not supported by PHP 5.6

--- tests/Bus/MessageBusSupportingMiddlewareAndDestructorTest.php
+++ tests/Bus/MessageBusSupportingMiddlewareAndDestructorTest.php
@@ -3,6 +3,7 @@
 namespace SimpleBus\Message\Tests\Bus;
 
 use SimpleBus\Message\Bus\Middleware\MessageBusSupportingMiddlewareAndDestructorFunction;
+use SimpleBus\Message\Tests\Bus\Fixtures\CallbackClass;
 
 class MessageBusSupportingMiddlewareAndDestructorTest extends \PHPUnit\Framework\TestCase
 {
@@ -13,29 +14,18 @@ class MessageBusSupportingMiddlewareAndDestructorTest extends \PHPUnit\Framework
      */
     public function it_invokes_the_callback_after_handling_middleware()
     {
-        $anonymousCallbackClass = new class{
-            /**
-             * @var string
-             */
-            public $locallyStoredMessage;
-
-            public function storeMessageLocally($messageToStoreLocally)
-            {
-                $this->locallyStoredMessage = $messageToStoreLocally;
-            }
-        };
-        $anonymousCallbackObject = new $anonymousCallbackClass();
+        $anonymousCallbackObject = new CallbackClass();
         $callback = array($anonymousCallbackObject, 'storeMessageLocally');
         $messageToStoreLocally = 'Hello World';
 
         $messageBusStack = new MessageBusSupportingMiddlewareAndDestructorFunction([], $callback, $messageToStoreLocally);
         unset($messageBusStack); //Trigger object destruction
-        $this->assertEquals($anonymousCallbackObject->locallyStoredMessage, null); //No message is handled yet, callback should not be invoked
+        $this->assertEquals($anonymousCallbackObject->locallyStoredMessage, null); //At this point no Message has been handled by the MessageBus yet: callback should NOT be invoked
 
         $messageBusStack = new MessageBusSupportingMiddlewareAndDestructorFunction([], $callback, $messageToStoreLocally);
         $messageBusStack->handle(new \stdClass());
         unset($messageBusStack); //Trigger object destruction
-        $this->assertEquals($anonymousCallbackObject->locallyStoredMessage, $messageToStoreLocally); //Message is handled, callback should be invoked
+        $this->assertEquals($anonymousCallbackObject->locallyStoredMessage, $messageToStoreLocally); //At this point a Message has been handled by the MessageBus: callback should be invoked
     }
 
 }
