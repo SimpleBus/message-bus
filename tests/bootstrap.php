879e31f3cf3a8bf3adda91d130906648c4933f4c
Cliff Odijk <cliff@jcid.nl>
2017-12-22 23:35:30 +0100

    Add recursive find for an autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+foreach ([__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../vendor/autoload.php'] as $file) {
+    if (file_exists($file)) {
+        require $file;
+        break;
+    }
+}
